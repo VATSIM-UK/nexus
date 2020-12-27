7c59632892da3e522e28222be168339173b953a3
Calum Towers <towers@hey.com>
2020-12-27 16:57:36 +0000

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
-<html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
   <head>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="We pride ourselves in providing regular and high quality air traffic control for our pilots as part of the VATSIM network.">
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
-    <script src="{{ mix('/js/app.js') }}" defer></script>
     <title>VATSIM UK - Nexus</title>
   </head>
   <body class="h-screen overflow-hidden bg-gray-100 dark:bg-gray-900">
     @inertia
+    <script src="{{ mix('/js/app.js') }}" defer></script>
   </body>
 </html>

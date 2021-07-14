8d98f831bace7cec8b3e3aabbc08374fd16cca2c
VATSIM UK <vatsim-uk-pa@users.noreply.github.com>
2021-07-14 13:06:34 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
 
@@ -20,13 +19,13 @@ class AuthController extends Controller
 
         // Authorise CID
         $whitelistedCids = config('auth.cid_whitelist');
-        if (!in_array($socialiteUser->getId(), $whitelistedCids)) {
+        if (! in_array($socialiteUser->getId(), $whitelistedCids)) {
             return redirect('/')->withErrors("You aren't able to sign in to Nexus at the moment.");
         }
 
         $user = User::updateOrCreate(
             [
-                'vatsim_cid' => $socialiteUser->getId()
+                'vatsim_cid' => $socialiteUser->getId(),
             ],
             [
                 'first_name' => $socialiteUser->first_name,
@@ -43,6 +42,7 @@ class AuthController extends Controller
     public function logout()
     {
         Auth::logout();
+
         return redirect('/');
     }
 }

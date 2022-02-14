1521b554a27c3a5ee826781dc6af5266bd1f8315
Calum Towers <towers@hey.com>
2022-02-14 21:58:37 +0000

    ci: Version Mix Assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,5 @@ let mix = require('laravel-mix');
 
 mix
   .js('resources/js/app.js', 'public/js')
-  .postCss('resources/css/app.css', 'public/css', [require('tailwindcss')]);
+  .postCss('resources/css/app.css', 'public/css', [require('tailwindcss')])
+  .version();
